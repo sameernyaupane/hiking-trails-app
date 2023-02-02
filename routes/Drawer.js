3513694cd8dad182bb1746030d60d4862286066b
Sameer Nyaupane <sameernyaupane@outlook.com>
2023-02-02 10:32:11 +0545

    Add authcontext to drawer route

--- routes/Drawer.js
+++ routes/Drawer.js
@@ -1,18 +1,22 @@
-import AboutScreen from "../screens/AboutScreen";
 import HomeStack from "./HomeStack"
-import { createDrawerNavigator } from "@react-navigation/drawer"
-import { NavigationContainer } from '@react-navigation/native';
+import AboutScreen from "../screens/AboutScreen";
+import {AuthContext} from '../context/AuthContext'
 import GroupsScreen from "../screens/GroupsScreen";
-import BudgetCalculation from "../screens/BudgetCalculation";
 import Recommendation from "../screens/Recommendation";
+import React, {useState, useContext, useEffect} from "react"
+import BudgetCalculation from "../screens/BudgetCalculation";
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from "@react-navigation/drawer"
 
 const RootDrawerNavigator = createDrawerNavigator();
 
 const Drawer = () => {
+  const [isLoading, userInfo, splashLoading] = useContext(AuthContext)
+
   return (
     <NavigationContainer>
       <RootDrawerNavigator.Navigator initialRouteName="HomeDrawer" options={{headerShown: false}}>
-        <RootDrawerNavigator.Screen name="Hiking Trails" component={HomeStack} options={{headerShown: true}}  />
+        <RootDrawerNavigator.Screen name="Hiking Trails" component={HomeStack} options={{headerShown: userInfo.token ? true : false}}  />
         <RootDrawerNavigator.Screen name="Groups" component={GroupsScreen} options={{headerShown: true}} />
         <RootDrawerNavigator.Screen name="Budget Calculation" component={BudgetCalculation} options={{headerShown: true}} />
         <RootDrawerNavigator.Screen name="Recommendation" component={Recommendation} options={{headerShown: true}} />
